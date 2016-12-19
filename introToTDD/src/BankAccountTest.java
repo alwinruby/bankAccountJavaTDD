3bcc5d91c52eb12ff6886eb8da4c35bbb7d80d8c
alwinruby <alwinsolanky@gmail.com>
2016-12-19 19:05:49 +0000

    multiple tests added

--- introToTDD/src/BankAccountTest.java
+++ introToTDD/src/BankAccountTest.java
@@ -5,13 +5,24 @@ import org.junit.Test;
 public class BankAccountTest {
 
 	@Test
-	public void test() {
+	public void testDeposit() {
 		BankAccount acc = new BankAccount();
 		acc.deposit(50);
 		assertEquals(acc.getBalance(), 50);
-		
-		acc.withdraw(30);
-		assertEquals(acc.getBalance(), 20);
+	}
+	
+	@Test
+	public void testWithdraw() {
+		BankAccount acc = new BankAccount(75);
+		acc.withdraw(50);
+		assertEquals(acc.getBalance(), 25);
+	}
+	
+	@Test
+	public void testWithdrawWithPenalty() {
+		BankAccount acc = new BankAccount(10);
+		acc.withdraw(20);
+		assertEquals(acc.getBalance(), -15);
 	}
 
 }
